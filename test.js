6a54d04cb6ab556ea7d7d8d81efb5f17211dad57
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-12 15:51:48 +0300

    Adding style to the HTML page

--- test.js
+++ test.js
@@ -1,18 +1,1 @@
 let myForm = document.getElementById("myForm");
-
-myForm.addEventListener("submit", (e) => {
-            e.preventDefault();
-
-            let request = new XMLHttpRequest();
-
-            request.open("post", "form.
-                php ");
-
-                request.onload = function() {
-                    console.log(request.responseText);
-                }
-
-                request.send(new FormData(myForm));
-            })
-
-        document.get
