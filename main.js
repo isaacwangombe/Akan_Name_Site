1d89876b8af9e8aed94203a451f2f5503f5e614d
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-12 09:53:58 +0300

    creating a function for day

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 let maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+let day = (year, month, date) => {}
+day()
