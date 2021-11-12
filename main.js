4b0c9ed61f07d03e47b544e6af55009185ed8c56
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-12 09:56:41 +0300

    Creating a switch statement to break down months into numbers

--- main.js
+++ main.js
@@ -3,5 +3,46 @@ let femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 let day = (year, month, date) => {
     DD = parseInt(date);
+
+    switch (month) {
+        case "January":
+            monthNumber = 0;
+            break;
+        case "February":
+            monthNumber = 1;
+            break;
+        case "March":
+            monthNumber = 2;
+            break;
+        case "April":
+            monthNumber = 3;
+            break;
+        case "May":
+            monthNumber = 4;
+            break;
+        case "June":
+            monthNumber = 5;
+            break;
+        case "July":
+            monthNumber = 6;
+            break;
+        case "August":
+            monthNumber = 7;
+            break;
+        case "September":
+            monthNumber = 8;
+            break;
+        case "October":
+            monthNumber = 9;
+            break;
+        case "November":
+            monthNumber = 12;
+            break;
+        case "December":
+            monthNumber = 11;
+            break;
+        default:
+            monthNumber = "invalid Month"
+    }
 }
 day()
